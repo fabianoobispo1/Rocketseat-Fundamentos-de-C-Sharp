a89a657f8e5c6a4d7af5126353a95ecb96b94c73
Fabiano Bispo Canedo <fabiano.canedo@energisa.com.br>
2024-05-29 09:44:27 -0300

    questoes finalizadas

---
+++ Questao02/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace questao02;
+class questao02
+{
+    static void Main(string[] args)
+    {
+
+        Console.Write("Digite seu primeiro nome: ");
+        string? primeiroNome = Console.ReadLine();
+
+        Console.Write("Digite seu sobrenome nome: ");
+        string? sobrenomeNome = Console.ReadLine();
+
+
+        Console.Write("\nNome completo:"+primeiroNome+" "+sobrenomeNome+".");
+
+        //ou 
+
+        //Console.Write($"Nome completo:{primeiroNome} {sobrenomeNome}.");
+        
+    }
+}
---
+++ Questao03/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace questao03;
+
+class questao03
+{
+    static void Main(string[] args)
+    {
+
+        Console.Write("Digite o primeiro valor: ");
+        string? vl1 = Console.ReadLine();
+        if (vl1 == null)
+        {
+            vl1 = "";
+        }
+        double Valor01 = 0;
+
+        if (Double.TryParse(vl1, out Valor01))
+        {
+            Valor01 = double.Parse(vl1);
+        }
+        else
+        {
+            Console.Write("Primeiro valor digitado e invalido.");
+            return;
+        }
+
+
+   
+        
+
+        Console.Write("Digite o segundo valor: ");
+        string? vl2 = Console.ReadLine();
+
+        if (vl2 == null)
+        {
+            vl2 = "";
+        }
+        double Valor02 = 0;
+
+        if (Double.TryParse(vl2, out Valor01))
+        {
+            Valor02 = double.Parse(vl2);
+        }
+        else
+        {
+            Console.Write("Primeiro valor digitado e invalido.");
+            return;
+        }
+
+
+        Console.WriteLine();
+        Console.WriteLine($"SOMA:{(Valor01 + Valor02)}");
+        Console.WriteLine($"SUBTRAÇÃO:{(Valor01 - Valor02)}");
+        Console.WriteLine($"MULTIPLICAÇÃO:{(Valor01 * Valor02)}");
+        if ((Valor01 == 0 && Valor02 == 0) || (Valor01 > 0 && Valor02 == 0))
+        {
+            Console.WriteLine("DIVISÃO: Não é possível dividir 0 para 0.");
+        }
+        else
+        {
+            Console.WriteLine($"DIVISÃO:{(Valor01 / Valor02)}");
+        }
+        Console.WriteLine($"MÉDIA:{((Valor01 + Valor02) / 2)}");
+
+    }
+}
